ef44de594b4ce6168eda4f50465956113c620d44
Syed Muhammad Haider <haiderali.swips@gmail.com>
2026-04-12 16:52:36 +0500

    Update Board.h

--- Board.h
+++ Board.h
@@ -2,7 +2,9 @@
 #define BOARD_H
 
 #include "Pieces.h"
-#include "Display.h"
+#include <string>
+
+using namespace std;
 
 // ── Board ─────────────────────────────────────────────────────────────────────
 
@@ -10,52 +12,25 @@ class Board {
     Piece* grid[8][8];
 
 public:
-    Board() {
-        for (int r = 0; r < 8; r++)
-            for (int c = 0; c < 8; c++)
-                grid[r][c] = nullptr;
-        init();
-    }
+    struct Position { 
+        int r, c; 
+    };
 
-    ~Board() {
-        for (int r = 0; r < 8; r++)
-            for (int c = 0; c < 8; c++)
-                delete grid[r][c];
-    }
+    struct MoveList {
+        Position moves[32];
+        int count = 0;
+    };
 
-    void init() {
-        // black back rank
-        grid[0][0] = new Rook("Black");
-        grid[0][1] = new Knight("Black");
-        grid[0][2] = new Bishop("Black");
-        grid[0][3] = new Queen("Black");
-        grid[0][4] = new King("Black");
-        grid[0][5] = new Bishop("Black");
-        grid[0][6] = new Knight("Black");
-        grid[0][7] = new Rook("Black");
+    Board();
+    ~Board();
 
-        // black pawns
-        for (int c = 0; c < 8; c++)
-            grid[1][c] = new Pawn("Black");
-
-        // white pawns
-        for (int c = 0; c < 8; c++)
-            grid[6][c] = new Pawn("White");
-
-        // white back rank
-        grid[7][0] = new Rook("White");
-        grid[7][1] = new Knight("White");
-        grid[7][2] = new Bishop("White");
-        grid[7][3] = new Queen("White");
-        grid[7][4] = new King("White");
-        grid[7][5] = new Bishop("White");
-        grid[7][6] = new Knight("White");
-        grid[7][7] = new Rook("White");
-    }
-
-    void display() {
-        displayBoard(grid);
-    }
+    void init();
+    void display();
+    
+    Piece* getPiece(int r, int c);
+    string getSquareName(int r, int c);
+    MoveList getValidMoves(int r, int c);
+    void makeMove(int fromR, int fromC, int toR, int toC);
 };
 
 #endif // BOARD_H
